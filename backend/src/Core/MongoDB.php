d580f1caad5d7b0ad7e08e047198d105a82bc143
maxroe66 <maxroe66300@gmail.com>
2025-12-20 18:11:57 +0100

    feat(core): ajout de la classe MongoDB utilitaire
    - Ajout d’une classe statique MongoDB pour centraliser la création du client MongoDB\Client
    - Utilisation de la configuration (URI) pour initialiser la connexion
    - Permet un accès simple et réutilisable à MongoDB dans les classes métier

---
+++ backend/src/Core/MongoDB.php
@@ -0,0 +1,9 @@
+
+<?php
+namespace App\Core;
+
+final class MongoDB {
+    public static function client(array $config): \MongoDB\Client {
+        return new \MongoDB\Client($config['mongo']['uri']);
+    }
+}
