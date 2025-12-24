4ec80b59881b68708938021c6eb2bf635ef034e5
maxroe66 <maxroe66300@gmail.com>
2025-12-24 09:49:16 +0100

    fix: strict_types en première ligne pour compatibilité PHP/CI

--- public/index.php
+++ public/index.php
@@ -1,9 +1,8 @@
 <?php
+declare(strict_types=1);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-declare(strict_types=1);
 
 use App\Core\Response;
 use App\Core\Router;
